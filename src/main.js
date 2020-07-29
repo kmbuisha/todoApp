af40616c23d2fb160f56b9c58860d142a8917e1d
Kevin <kmkevinmbuyi@yahoo.com>
2020-07-29 10:49:29 +0200

    using json placeholder and axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import uuid from 'vue-uuid'
-  
+import axios from 'axios'
+import VueAxios from 'vue-axios' 
+
+Vue.use(VueAxios, axios);
 Vue.use(uuid);
 Vue.config.productionTip = false
 
